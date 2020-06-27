bba4d80a8f57155f65f15ae4e80f3e3d7e5bf183
Jason Honor <jasonhonor@outlook.com>
2020-06-28 01:07:48 +0800

    1. 部署脚本测试通过 2. 发布脚本增加了部署脚本模板

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 go build -ldflags="-s -w -X 'main.BuildID=$1'" -o gfx
 scp ./gfx root@192.168.61.11:/opt/syscenter/syscenter
-
+scp ./template/deploy.html root@192.168.61.11:/opt/syscenter/template/deploy.html
